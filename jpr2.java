b9c110826013b13298da3f310ba72ae70bfa16e6
vaibhaviDixit <vaibhavidixit511@gmail.com>
2021-08-10 17:50:54 +0530

    10/8/21 Assignment

---
+++ jpr2.java
@@ -0,0 +1,7 @@
+package com.google;
+
+public class jpr2 {
+    public static void main(String[] args) {
+        System.out.println(" \" can we print '\\' with System.out.println() statement? \" ");
+    }
+}

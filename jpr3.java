bc66a6b26763069179fe18678024ecb44aabb006
vaibhaviDixit <vaibhavidixit511@gmail.com>
2021-08-11 09:34:33 +0530

    Assignment 3 and 4

---
+++ jpr3.java
@@ -0,0 +1,14 @@
+package com.google;
+
+public class jpr3 {
+    static int default=5;
+    static String public="main";
+    Static int k=89;
+    public static void main(String[] args) {
+
+        System.out.println(default);// error: cant give varibale name as reserved keyword
+        System.out.println(public);// error: cant give varibale name as reserved keyword
+        System.out.println(k);// error: java is case sensitive
+
+    }
+}
---
+++ jpr4.java
@@ -0,0 +1,16 @@
+package com.google;
+
+public class jpr4 {
+    static String s;
+    static int a;
+    static double d;
+    static float f;
+    int score=80;
+    public static void main(String[] args) {
+        System.out.println(s);
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(score);//Error cannot access nonstatic from static
+    }
+}

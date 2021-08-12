6b34c02ca0d3b728f5349916434faca3229cb687
vaibhaviDixit <vaibhavidixit511@gmail.com>
2021-08-12 09:31:05 +0530

    5th Assignment

---
+++ JPR5.java
@@ -0,0 +1,14 @@
+package com.google;
+
+public class JPR5 {
+
+    public static void main(String[] args) {
+        System.out.println("Byte: "+Byte.MAX_VALUE+" "+Byte.MIN_VALUE+" "+Byte.SIZE);
+        System.out.println("Short: "+Short.MAX_VALUE+" "+Short.MIN_VALUE+" "+Short.SIZE);
+        System.out.println("Integer: "+Integer.MAX_VALUE+" "+Integer.MIN_VALUE+" "+Integer.SIZE);
+        System.out.println("Float: "+Float.MAX_VALUE+" "+Float.MIN_VALUE+" "+Float.SIZE);
+        System.out.println("Double: "+Double.MAX_VALUE+" "+Double.MIN_VALUE+" "+Double.SIZE);
+        System.out.println("Character: "+Character.MAX_VALUE+" "+Character.MIN_VALUE+" "+Character.SIZE);
+
+    }
+}

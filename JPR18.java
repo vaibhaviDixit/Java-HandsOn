96f3016244e8ac68f7ee52d0988ffb1f4d0a938d
vaibhaviDixit <vaibhavidixit511@gmail.com>
2021-08-14 12:20:41 +0530

    added comments

--- JPR18.java
+++ JPR18.java
@@ -7,22 +7,29 @@ public class JPR18 {
         int amount=161258;
 
         float EMI;
-        //calculating emi for 5 years:
-        float r=(5.0f/(12*100));
+        //calculating emi for principal  = 161258 , years=5 years, rate=5% per month:
+        //        EMI = P × r × (1 + r)^n / ((1 + r)^n - 1)
+        //        where P= Loan amount, r= interest rate, n=tenure in number of months.
+        //n= 5 years*12=60 months
+
+        float r=(5.0f/100);
         float rpl1=1+r;
         float numerator= (float) (amount*r*Math.pow(rpl1,60));
         float denom= (float) (Math.pow(rpl1,60)-1);
         EMI=numerator/denom;
         System.out.println("EMI for amount "+amount+" of "+y1+" Years is: "+EMI);
-        //calculating EMI for 3 years:
-        r=(5.0f/(12*100));
+
+        //calculating emi for principal  = 161258 , years=3 years, rate=5% per month:
+        //        EMI = P × r × (1 + r)^n / ((1 + r)^n - 1)
+        //        where P= Loan amount, r= interest rate, n=tenure in number of months.
+        //n= 3 years*12=36 months
+
+        r=(5.0f/100);
         rpl1=1+r;
         numerator= (float) (amount*r*Math.pow(rpl1,36));
         denom= (float) (Math.pow(rpl1,36)-1);
         EMI=numerator/denom;
-        System.out.println("EMI for amount "+amount+" of "+y2+" Years is: "+EMI);
-
-
+        System.out.println("EMI for amount "+amount+" of "+y1+" Years is: "+EMI);
 
 
 
